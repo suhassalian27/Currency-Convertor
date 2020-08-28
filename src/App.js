b41763d5d10b9017420bb7ac012ce54c8f4c197c
suhas <suhas.salian27@gmail.com>
2020-08-28 19:13:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CONVERTOR from '.components/Convertor'
+
+function App() {
+    return (
+        <div className="App">
+            <CONVERTOR />
+        </div>
+    );
+}
+
+export default App;
